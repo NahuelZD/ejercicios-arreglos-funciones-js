a3c68be7611979296c2d1b7150b4cbb53ec15e0f
Francisco Nahuel Zelaya <nahuelmassa93@gmail.com>
2022-09-02 15:03:38 -0300

    ejercicios 3 y 4 de funciones andando

--- script/function.js
+++ script/function.js
@@ -36,3 +36,42 @@ function borrarCadena(){
     let mensaje = document.getElementById('mostrarMayusMinus');
     mensaje.innerHTML = '';
 }
+
+// Ejercicio 3
+
+function calcularPerimetro(){
+    let base = Number(document.getElementById('baseRectangulo').value);
+    let altura = Number(document.getElementById('alturaRectangulo').value);
+    let mensaje = document.getElementById('mostrarPerimetro');
+
+    mensaje.innerHTML = `El perímetro del rectángulo es ${(base+altura)*2}`
+}
+
+function borrarPerimetro(){
+    let mensaje = document.getElementById('mostrarPerimetro');
+    mensaje.innerHTML = '';
+}
+
+// Ejercicio 4
+
+function multiplicar(){
+    let tabla = document.getElementById('tablaMulti');
+    let numero = document.getElementById('numeroMulti').value;
+    tabla.innerHTML = '';
+
+    for(let i = 1; i <= 10; i++){
+        let tr = document.createElement('tr');
+        let tdMulti = document.createElement('td');
+        let tdTotal = document.createElement('td');
+        tdMulti.innerHTML = `${i} x ${numero} -->`;
+        tdTotal.innerHTML = `${i*numero}`;
+        tr.appendChild(tdMulti);
+        tr.appendChild(tdTotal);
+        tabla.appendChild(tr);
+    }
+}
+
+function borrarTabla(){
+    let tabla = document.getElementById('tablaMulti');
+    tabla.innerHTML = '';
+}
